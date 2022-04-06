3acb11b96e35d3acf23cc3e98bdb94cfa71e15e2
Ayed Nejmeddine <ayednejmeddine53@gmail.com>
2022-04-06 11:54:18 +0100

    add SectionList

--- App.js
+++ App.js
@@ -3,35 +3,42 @@ import {
   View,
   Text,
   StyleSheet,
-  ScrollView,
   RefreshControl,
-  FlatList,
+  SectionList,
 } from 'react-native';
 
 const App = () => {
   const [refresh, setRefresh] = useState(false);
   const [items, setItems] = useState([
-    {item: 'item 1'},
-    {item: 'item 2'},
-    {item: 'item 3'},
-    {item: 'item 4'},
-    {item: 'item 5'},
-    {item: 'item 6'},
-    {item: 'item 7'},
+    {title: 'Title 1', data: ['item 1-1', 'item 1-2', 'item 1-3']},
+    {title: 'Title 2', data: ['item 2-1', 'item 2-2']},
+    {title: 'Title 3', data: ['item 3-1', 'item 3-2', 'item 3-3']},
   ]);
 
   const onRefreshItem = () => {
     setRefresh(true);
-    setItems([...items, {item: 'item 8'}]);
+    setItems([
+      ...items,
+      {
+        title: 'Title 4',
+        data: ['item 4-1', 'item 4-2', 'item 4-3', 'item 4-4'],
+      },
+    ]);
     setRefresh(false);
   };
 
   return (
-    <FlatList
-      data={items}
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={items}
       renderItem={({item}) => (
         <View style={styles.item}>
-          <Text style={styles.text}>{item.item}</Text>
+          <Text style={styles.text}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({section}) => (
+        <View style={styles.title}>
+          <Text style={styles.text}>{section.title}</Text>
         </View>
       )}
       refreshControl={
@@ -55,11 +62,16 @@ const styles = StyleSheet.create({
     color: '#000000',
     fontWeight: 'bold',
   },
-  item: {
+  title: {
     backgroundColor: '#4ae1fa',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 10,
+    marginTop: 10,
+  },
+  item: {
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
