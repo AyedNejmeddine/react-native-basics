22fdefa863842f5c5faab33820d24b7b2e4db040
Ayed Nejmeddine <ayednejmeddine53@gmail.com>
2022-04-06 11:19:05 +0100

    add FlatList

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, ScrollView, RefreshControl} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+} from 'react-native';
 
 const App = () => {
   const [refresh, setRefresh] = useState(false);
   const [items, setItems] = useState([
-    {key: 1, item: 'item 1'},
-    {key: 2, item: 'item 2'},
-    {key: 3, item: 'item 3'},
-    {key: 4, item: 'item 4'},
-    {key: 5, item: 'item 5'},
-    {key: 6, item: 'item 6'},
-    {key: 7, item: 'item 7'},
+    {item: 'item 1'},
+    {item: 'item 2'},
+    {item: 'item 3'},
+    {item: 'item 4'},
+    {item: 'item 5'},
+    {item: 'item 6'},
+    {item: 'item 7'},
   ]);
 
   const onRefreshItem = () => {
     setRefresh(true);
-    setItems([...items, {key: 8, item: 'item 8'}]);
+    setItems([...items, {item: 'item 8'}]);
     setRefresh(false);
   };
 
   return (
-    <View style={styles.body}>
-      <ScrollView
-        refreshControl={
-          <RefreshControl refreshing={refresh} onRefresh={onRefreshItem} />
-        }>
-        {items.map(object => {
-          return (
-            <View style={styles.item} key={object.key}>
-              <Text style={styles.text}>{object.item}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
-    </View>
+    <FlatList
+      data={items}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.item}</Text>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefreshItem} />
+      }
+    />
   );
 };
 
