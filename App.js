bff27ddae50963f161922941bf2697aac17765c4
Ayed Nejmeddine <ayednejmeddine53@gmail.com>
2022-04-07 00:13:10 +0100

    add modal with animation

--- App.js
+++ App.js
@@ -7,35 +7,48 @@ import {
   Pressable,
   Alert,
   ToastAndroid,
+  Modal,
 } from 'react-native';
 
 const App = () => {
   const [name, setName] = useState('');
   const [submitted, setSabmitted] = useState(false);
+  const [showWornig, setShowWornig] = useState(false);
 
   const onPressHandler = () => {
     if (name.length > 3) {
       setSabmitted(!submitted);
     } else {
-      Alert.alert(
-        'warning',
-        'the length of name must be longer then 3 caracteres',
-        [
-          {text: 'cancel', onPress: () => console.warn('on press cancel')},
-          {text: 'ok', onPress: () => console.warn('on press ok')},
-        ],
-        {
-          cancelable: true,
-          onDismiss: () => {
-            ToastAndroid.show('click outside the Alert', ToastAndroid.SHORT);
-          },
-        },
-      );
+      setShowWornig(true);
     }
   };
 
   return (
     <View style={styles.body}>
+      <Modal
+        transparent
+        visible={showWornig}
+        animationType="fade"
+        hardwareAccelerated
+        onRequestClose={() => setShowWornig(false)}>
+        <View style={styles.center_view}>
+          <View style={styles.warnin_modal}>
+            <View style={styles.warnin_title}>
+              <Text style={styles.title}>WARNING !</Text>
+            </View>
+            <View style={styles.body_modal}>
+              <Text style={styles.body_text}>
+                the length of name must be longer then 3 caracteres
+              </Text>
+            </View>
+            <Pressable
+              style={styles.warning_button}
+              onPress={() => setShowWornig(false)}>
+              <Text style={styles.body_text}>OK</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.text}>Write your name here</Text>
       <TextInput
         multiline
@@ -86,6 +99,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 50,
+  },
+  warnin_modal: {
+    height: 400,
+    width: 400,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 30,
+  },
+  center_view: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00000099',
+  },
+  warnin_title: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff0',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  body_modal: {
+    height: 260,
+    padding: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  body_text: {
+    fontSize: 20,
+    color: '#000',
+    textAlign: 'center',
+  },
+  warning_button: {
+    height: 70,
+    justifyContent: 'center',
+    backgroundColor: '#00ffff',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
   },
 });
 
