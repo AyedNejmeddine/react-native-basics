0e9f41fe86c46254e6a194d400fdcc3a61fb095e
Ayed Nejmeddine <ayednejmeddine53@gmail.com>
2022-04-07 15:31:02 +0100

    use navigation screen

--- App.js
+++ App.js
@@ -1,89 +1,63 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  Pressable,
-  Modal,
-  Image,
-  ImageBackground,
-} from 'react-native';
-import CustomButton from './src/CustomButton';
-import Header from './src/Header';
+import React from 'react';
+import {View, Text, StyleSheet, Pressable as Guessable} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-const App = () => {
-  const [name, setName] = useState('');
-  const [submitted, setSabmitted] = useState(false);
-  const [showWornig, setShowWornig] = useState(false);
-
-  const onPressHandler = () => {
-    if (name.length > 3) {
-      setSabmitted(!submitted);
-    } else {
-      setShowWornig(true);
-    }
+function ScreenA({navigation}) {
+  const onPresHandler = () => {
+    navigation.navigate('Screen B');
   };
 
   return (
-    <ImageBackground
-      style={styles.body}
-      source={{
-        uri: 'https://i.pinimg.com/originals/f0/40/f0/f040f07ac0ad09cc155ecc4bbface15a.jpg',
-      }}>
-      <Header />
-      <Modal
-        transparent
-        visible={showWornig}
-        animationType="fade"
-        hardwareAccelerated
-        onRequestClose={() => setShowWornig(false)}>
-        <View style={styles.center_view}>
-          <View style={styles.warnin_modal}>
-            <View style={styles.warnin_title}>
-              <Text style={styles.title}>WARNING !</Text>
-            </View>
-            <View style={styles.body_modal}>
-              <Text style={styles.body_text}>
-                the length of name must be longer then 3 caracteres
-              </Text>
-            </View>
-            <Pressable
-              style={styles.warning_button}
-              onPress={() => setShowWornig(false)}>
-              <Text style={styles.body_text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}>Write your name here</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder="e.g. Nejmeddine"
-        onChangeText={value => setName(value)}
-      />
-      <CustomButton
-        onPressHandler={onPressHandler}
-        title={submitted ? 'Clear' : 'Submit'}
-      />
-      {submitted ? (
-        <View style={styles.body}>
-          <Text style={styles.text}>your are registred as : {name}</Text>
-          <Image
-            style={styles.image}
-            source={require('./assets/done.png')}
-            resizeMode="stretch"
-          />
-        </View>
-      ) : (
-        <Image
-          style={styles.image}
-          source={require('./assets/error.png')}
-          resizeMode="stretch"
+    <View style={styles.body}>
+      <Text style={styles.text}>from screen A</Text>
+      <Guessable
+        style={({pressed}) => [
+          {backgroundColor: pressed ? '#0d0' : '#0f0'},
+          styles.button,
+        ]}
+        onPress={onPresHandler}>
+        <Text style={styles.text}>Go to a screen B</Text>
+      </Guessable>
+    </View>
+  );
+}
+
+function ScreenB({navigation}) {
+  const onPresHandler = () => {
+    // navigation.navigate('Screen A');
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>from screen B</Text>
+      <Guessable
+        style={({pressed}) => [
+          {backgroundColor: pressed ? '#0d0' : '#0f0'},
+          styles.button,
+        ]}
+        onPress={onPresHandler}>
+        <Text style={styles.text}>Go back to a screen A</Text>
+      </Guessable>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen
+          // options={{header: () => null}}
+          name="Screen A"
+          component={ScreenA}
         />
-      )}
-    </ImageBackground>
+        <Stack.Screen name="Screen B" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -91,81 +65,20 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     fontSize: 30,
     margin: 10,
     color: '#000000',
   },
-  input: {
-    width: 400,
-    fontSize: 25,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 10,
-    textAlign: 'center',
-    marginBottom: 25,
-  },
   button: {
     height: 80,
-    width: 200,
+    width: 400,
     margin: 20,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 50,
-  },
-  warnin_modal: {
-    height: 400,
-    width: 400,
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 30,
-  },
-  center_view: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#00000099',
-  },
-  warnin_title: {
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ff0',
-    borderWidth: 1,
-    borderColor: '#999',
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  body_modal: {
-    height: 260,
-    padding: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  body_text: {
-    fontSize: 20,
-    color: '#000',
-    textAlign: 'center',
-  },
-  warning_button: {
-    height: 70,
-    justifyContent: 'center',
-    backgroundColor: '#00ffff',
-    borderWidth: 1,
-    borderColor: '#999',
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-  },
-  image: {
-    height: 100,
-    width: 100,
   },
 });
 
